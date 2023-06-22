02f2977cb444aa8a11ffa1f34ddd83bbd3f6cdb7
vict-miranda <54920837+vict-miranda@users.noreply.github.com>
2023-06-22 11:58:59 -0400

    fix asd

--- TiendaServicios.API.Autor/Models/GradoAcademico.cs
+++ TiendaServicios.API.Autor/Models/GradoAcademico.cs
@@ -3,11 +3,11 @@
     public class GradoAcademico
     {
         public int GradoAcademicoId { get; set; }
-        public string Nombre { get; set; }
-        public string CentroAcademico { get; set;}
+        public string Nombre { get; set; }  = string.Empty;
+        public string CentroAcademico { get; set; } = string.Empty;
         public DateTime? FechaGrado { get; set; }
         public int AutorLibroId { get; set; }
-        public AutorLibro AutorLibro { get; set; }
-        public string GradoAcademicoGuid { get; set; }
+        public AutorLibro? AutorLibro { get; set; }
+        public string GradoAcademicoGuid { get; set; } = string.Empty;
     }
 }
--- TiendaServicios.API.Libro/Models/LibreriaMaterial.cs
+++ TiendaServicios.API.Libro/Models/LibreriaMaterial.cs
@@ -3,7 +3,7 @@
     public class LibreriaMaterial
     {
         public Guid? LibreriaMaterialId { get; set; }
-        public string Titulo { get; set; }
+        public string Titulo { get; set; } = string.Empty;
         public DateTime? FechaPublicacion { get; set; }
         public Guid? AutorLibroId { get; set; }
     }
